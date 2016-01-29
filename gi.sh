fab4de76c4958616e8e9ff617663195cd1186eec
Diomidis Spinellis <dds@aueb.gr>
2016-01-29 07:55:41 +0200

    Fix comment

--- gi.sh
+++ gi.sh
@@ -2,20 +2,20 @@
 #
 # (C) Copyright 2016 Diomidis Spinellis
 #
-# This file is part of  , the Git-based issue management system.
+# This file is part of gi, the Git-based issue management system.
 #
-#   is free software: you can redistribute it and/or modify
+# gi is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-#   is distributed in the hope that it will be useful,
+# gi is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with CScout.  If not, see <http://www.gnu.org/licenses/>.
+# along with gi.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 # Display system usage and exit
