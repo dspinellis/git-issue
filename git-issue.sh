98ea90195e6a405e217e0340dc585a7058b3ab2a
Diomidis Spinellis <dds@aueb.gr>
2018-07-13 12:17:35 +0300

    Restore in-place sed invocation for GNU sed

--- git-issue.sh
+++ git-issue.sh
@@ -128,7 +128,7 @@ edit()
   cp "$file" "$file.new"
   echo "Opening editor..."
   ${VISUAL:-vi} "$file.new" || return 1
-  sed -i .original '/^#/d' "$file.new"
+  sed -i.original '/^#/d' "$file.new"
   rm -f "$file.new.original"
   if [ $(grep -c . "$file.new") -eq 0 ] ; then
     echo 'Empty file' 1>&2
