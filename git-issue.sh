7c4b07d15816b8e6d04504e7f7194afd97136688
Diomidis Spinellis <dds@aueb.gr>
2018-11-01 20:59:55 +0200

    Simplify test for empty file

--- git-issue.sh
+++ git-issue.sh
@@ -147,7 +147,7 @@ edit()
   ${VISUAL:-vi} "$file.new" || return 1
   sed -i.original '/^#/d' "$file.new"
   rm -f "$file.new.original"
-  if [ $(grep -c . "$file.new") -eq 0 ] ; then
+  if ! test -s "$file.new" ; then
     echo 'Empty file' 1>&2
     rm -f "$file.new"
     return 1
